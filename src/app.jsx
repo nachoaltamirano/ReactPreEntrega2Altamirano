16dbe7f3bd69a194c336e90e835321b41010b69d
Nacho <nachoaltamiran@gmail.com>
2023-05-24 16:39:23 -0300

    segunda entrega

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,30 @@
 import ItemDetailContainer from "./components/ItemDetailContainer";
 import ItemListContainer from "./components/ItemListContainer";
-import Navbar from "./components/Navbar";
-import "./main.css"
-
+import NavBar from "./components/NavBar";
+import { BrowserRouter , Route , Routes  } from "react-router-dom";
 function App() {
 
   return (
     <div>
-      <Navbar />
+      <BrowserRouter>
+
+
+      <NavBar />
+
+      <Routes>
+        <Route path="/" element={<ItemListContainer/>} />
+        <Route path="item/:id" element={<ItemDetailContainer itemId={2}/>} />
+        <Route path="/nosotros" element={<Nosotros />}/>
+      </Routes>
+
+
+
       <ItemListContainer />
+
+
       <ItemDetailContainer itemId={2} />
+    
+    </BrowserRouter>
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const Navbar = () => {
+const NavBar = () => {
   return (
     <nav className="navbar">
-        <a href="#" className="logo"><h1>Carpishop</h1></a>
+        <Link href="/" className="logo"><h1>TODOFUTBOL</h1></a>
         <ul className="menu">
-            <li><a className="menu-link" href="#">Inicio</a></li>
-            <li><a className="menu-link" href="#">Nosotros</a></li>
-            <li><a className="menu-link" href="#">Productos</a></li>
-            <li><a className="menu-link" href="#">Contacto</a></li>
+            <li><Link className="menu-link" href="#">Inicio</Link></li>
+            <li><Link className="menu-link" href="/nosotros">Nosotros</Link></li>
+            <li><Link className="menu-link" href="#">Productos</Link></li>
+            <li><Link className="menu-link" href="#">Contacto</Link></li>
         </ul>
     </nav>
   )
 }
 
-export default Navbar
+export default NavBar
